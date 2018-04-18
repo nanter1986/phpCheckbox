418ef2560f9ed599e720fde325d78d4085c72c33
nanter1986 <nanter1986@gmail.com>
2018-04-18 13:38:45 +0300

    working checkbox

---
+++ confirm.php
@@ -0,0 +1,4 @@
+<?php
+$myA=$_POST['myarray'];
+echo $myA[0];
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<form action='confirm.php' method='post'>
+<label>nanter</label>
+<input type='checkbox' value='nanter' name='myarray[]'>
+<br>
+
+<label>alina</label>
+<input type='checkbox' value='alina' name='myarray[]'>
+<br>
+
+<label>themis</label>
+<input type='checkbox' value='themis' name='myarray[]'>
+<br>
+
+<input type='submit' value='confirm'>
+<br>
+
+</form>
