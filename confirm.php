bd81a6886696536ad46081ebdc3c63c89e61d9e2
nanter1986 <nanter1986@gmail.com>
2018-04-19 12:53:09 +0300

    new table,rezults show in page

--- confirm.php
+++ confirm.php
@@ -4,4 +4,23 @@ foreach($myA as $a){
 	echo $a . "<br>";
 
 }
+class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('names.db');
+      }
+   }
+   
+   $db = new MyDB();
+   if(!$db){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+$result=$db->query('select * from people');
+while($row=$result->fetchArray(SQLITE3_ASSOC)){
+	echo $row['id'] . ".";
+	echo $row['name'] . "-";
+	echo $row['age'] . "<br>";
+
+}
 ?>
